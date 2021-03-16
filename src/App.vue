42a45da40642187f620d4d38f76fac80d68832c0
Pawan deore <pavandeore90@gmail.com>
2021-03-16 22:21:31 +0530

    fresh deploy try

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NavBar />
+    <div id="beer-wraper">
+      <BeerComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import NavBar from './components/Navbar';
+import BeerComponent from './components/BeerComponent';
+
+export default {
+  name: 'App',
+  components: {
+    BeerComponent,
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+*{
+  margin:0;
+  padding:0;
+  font-family: verdana;
+  cursor: url('./assets/beer.png'), auto;
+}
+#beer-wraper > div{
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+</style>
